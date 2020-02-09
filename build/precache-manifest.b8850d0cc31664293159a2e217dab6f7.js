4c61c6c904e4d965ce978e0733fcb1171267c7fa
Arhanechka <aranvic1976@gmail.com>
2020-02-10 01:41:21 +0200

    build

---
+++ build/precache-manifest.b8850d0cc31664293159a2e217dab6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2ec770eabe13e5c5e177fdd3ad316c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01ef038048cba9861487",
+    "url": "/static/css/main.3e6b17ce.chunk.css"
+  },
+  {
+    "revision": "5a4ef2a7736c06a04a95",
+    "url": "/static/js/2.0b474abe.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0b474abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ef038048cba9861487",
+    "url": "/static/js/main.2ac7a6cd.chunk.js"
+  },
+  {
+    "revision": "d5ca5dc0d9f42c5a9290",
+    "url": "/static/js/runtime-main.44762092.js"
+  }
+]);
